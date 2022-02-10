c9937ae37e45d491c07f28f1c8aceb6b7dd09107
White <White@DESKTOP-A3DD369>
2022-02-10 10:44:21 +0300

    Добавьте файлы проекта.

---
+++ parser/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace parser
+{
+    partial class Science_RU_day_Xak
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.start.Location = new System.Drawing.Point(12, 53);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(269, 65);
+            this.start.TabIndex = 0;
+            this.start.Text = "Начать";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Parser";
+            // 
+            // Science_RU_day_Xak
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 138);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.start);
+            this.Name = "Science_RU_day_Xak";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ parser/Form1.cs
@@ -0,0 +1,232 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace parser
+{
+    public partial class Science_RU_day_Xak : Form
+    {
+        public Science_RU_day_Xak()
+        {
+            InitializeComponent();
+        }
+
+        private static IWebDriver driver = null;
+
+
+        void Log(object x)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("[" + DateTime.Now + "]  " + x);
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        void SUCCESSLog(object x)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("[" + DateTime.Now + "]  " + x);
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+        void ERRORLog(object x)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[" + DateTime.Now + "]  " + x);
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        class pars
+        {
+            public string _title { get; set; }
+            public object _avtor { get; set; }
+            public string _ISBN { get; set; }
+            public string _type { get; set; }
+            public string _papent_number { get; set; }
+
+        }
+        private void start_Click(object sender, EventArgs e)
+        {
+
+            main();
+        }
+
+
+
+        void main()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("'excludeSwitches', ['enable-logging']");
+            options.AddArgument("--log-level=3");
+            options.AddArgument("--window-size=1920,920");
+            options.AddArgument("--disable-blink-features=AutomationControlled");
+            options.AddExcludedArgument("enable-automation");
+            IWebDriver driver = new ChromeDriver(options);
+            Excel.Application excel_app = new Excel.Application();
+            excel_app.Visible = true;
+            excel_app.Workbooks.Add();
+            Excel._Worksheet sheet = excel_app.ActiveSheet;
+            sheet.Cells[1, 1] = "Ссылка на статью";
+            sheet.Cells[1, 2] = "Название";
+            sheet.Cells[1, 3] = "ТИП ИЗДАНИЯ";
+            sheet.Cells[1, 4] = "ЯЗЫК";
+            sheet.Cells[1, 5] = "Тип публикации";
+            driver.Navigate().GoToUrl("https://www.elibrary.ru/org_items.asp?orgsid=1020%22");
+            Thread.Sleep(1000);
+            int row = 2;
+            int stranica = 4; // 1 страница на сайте в хпатхе == 3
+            Go();
+            void Go()
+            {
+                for (int id = 4; id <= 103; id++)
+                {
+                    if (id == 103)
+                    {
+
+
+                        next_page();
+                        Go();
+                    }
+                    try
+                    {
+                        IWebElement element = driver.FindElement(By.XPath($"/html/body/div[3]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/table/tbody/tr[2]/td[1]/table/tbody/tr/td/table/tbody/tr[{id}]"));
+                        Log($"Post {id}:" + element.Text);
+                        IWebElement href = driver.FindElement(By.XPath($"/html/body/div[3]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/table/tbody/tr[2]/td[1]/table/tbody/tr/td/table/tbody/tr[{id}]/td[2]/a"));
+                        string link = href.GetAttribute("href");
+                        SUCCESSLog(link);
+                        sheet.Cells[row, 1] = link;
+                        try
+                        {
+
+                            href.Click();
+                            Thread.Sleep(500);
+                            try
+                            {
+                                string Название = driver.FindElement(By.XPath("/html/body/table/tbody/tr/td/table[1]/tbody/tr/td[2]/table/tbody/tr[2]/td[1]/table[2]/tbody/tr/td[2]/span/b/p")).Text;
+                                sheet.Cells[row, 2] = Название;
+                            }
+                            catch { sheet.Cells[row, 2] = "Ошибка получения"; }
+                            try
+                            {
+                                string Тип = driver.FindElement(By.XPath("/html/body/table/tbody/tr/td/table[1]/tbody/tr/td[2]/table/tbody/tr[2]/td[1]/div/table[2]/tbody/tr[1]/td/font[1]")).Text;
+                                sheet.Cells[row, 3] = Тип;
+
+                            }
+                            catch { sheet.Cells[row, 3] = "Ошибка получения"; }
+                            try
+                            {
+                                string Язык = driver.FindElement(By.XPath("/html/body/table/tbody/tr/td/table[1]/tbody/tr/td[2]/table/tbody/tr[2]/td[1]/div/table[3]/tbody/tr[1]/td/font[2]")).Text;
+                                sheet.Cells[row, 4] = Язык;
+                            }
+                            catch { sheet.Cells[row, 4] = "Ошибка получения"; }
+
+                            try
+                            {
+                                string ISBN = driver.FindElement(By.XPath("/html/body/table/tbody/tr/td/table[1]/tbody/tr/td[2]/table/tbody/tr[2]/td[1]/div/table[3]/tbody/tr[1]/td/font[3]")).Text;
+                                sheet.Cells[row, 5] = "Статья, ISBN: " + ISBN;
+                            }
+                            catch
+                            {
+                                try
+                                {
+                                    string ISBN_2 = driver.FindElement(By.XPath("/html/body/table/tbody/tr/td/table[1]/tbody/tr/td[2]/table/tbody/tr[2]/td[1]/div/table[2]/tbody/tr[1]/td/font[3]")).Text;
+                                    sheet.Cells[row, 5] = "Статья, ISBN: " + ISBN_2;
+                                }
+                                catch 
+                                {
+                                    try
+                                    {
+                                        
+                                        //string contains_patent = driver.FindElement(By.XPath("/html/body/table/tbody/tr/td/table[1]/tbody/tr/td[2]/table/tbody/tr[2]/td[1]/div/table[2]/tbody/tr[1]/td/text()[2]")).Text;
+                                        string patent_number = driver.FindElement(By.XPath("/html/body/table/tbody/tr/td/table[1]/tbody/tr/td[2]/table/tbody/tr[2]/td[1]/div/table[2]/tbody/tr[2]/td/font[1]")).Text;
+                                        sheet.Cells[row, 5] = "Патент: " + patent_number;
+
+
+                                    }
+                                    catch
+                                    {
+                                        string contains_patent = driver
+                                            .FindElement(By.XPath("/html/body/table/tbody/tr/td/table[1]/tbody/tr/td[2]/table/tbody/tr[2]/td[1]/div/table[2]/tbody/tr[1]/td/text()[2]")).Text;
+                                        if (contains_patent.Contains("патент"))
+                                        {
+                                            string patent_number_2 = driver.FindElement(By.XPath("/html/body/table/tbody/tr/td/table[1]/tbody/tr/td[2]/table/tbody/tr[2]/td[1]/div/table[2]/tbody/tr[1]/td/font[2]")).Text;
+                                            sheet.Cells[row, 5] = "Патент: " + patent_number_2;
+                                        }
+                                    }
+
+                                }
+
+                            }
+
+                            driver.Navigate().Back();
+
+                            Thread.Sleep(500);
+
+                        }
+                        catch
+                        {/* MessageBox.Show("1")*/
+                            ; driver.Navigate().Back();
+                        }
+
+
+
+                        row++;
+
+
+
+                    }
+                    catch
+                    {
+                        Console.WriteLine("С этой страницей все");
+                        try
+                        {
+
+                            IWebElement next = driver.FindElement(By.XPath(
+                                $"/html/body/div[3]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/table/tbody/tr[2]/td[1]/table/tbody/tr/td/div[4]/table/tbody/tr/td[13]"));
+                            next.Click();
+                            id = 4;
+                            Go();
+
+                        }
+                        catch (Exception ex)
+                        {
+
+                            ERRORLog(ex.Message);
+                            IWebElement next = driver.FindElement(By.XPath($"/html/body/div[2]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/table/tbody/tr[2]/td[1]/table/tbody/tr/td/div[4]/table/tbody/tr/td[13]"));
+                            next.Click();
+                            id = 4;
+                            Go();
+
+
+
+                        }
+                    }
+
+
+                }
+            }
+
+            void next_page()
+            {
+                try
+                {
+                    stranica++;
+                    IWebElement next = driver.FindElement(By.XPath($"/html/body/div[3]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/table/tbody/tr[2]/td[1]/table/tbody/tr/td/div[4]/table/tbody/tr/td[13]"));
+                    next.Click();
+                }
+                catch (Exception ex)
+                {
+                    ERRORLog(ex.Message);
+                    IWebElement next = driver.FindElement(By.XPath($"/html/body/div[2]/table/tbody/tr/td/table[1]/tbody/tr/td[2]/form/table/tbody/tr[2]/td[1]/table/tbody/tr/td/div[4]/table/tbody/tr/td[13]"));
+                    next.Click();
+                    Go();
+                }
+
+            }
+
+
+
+        }
+    }
+
+
+}
